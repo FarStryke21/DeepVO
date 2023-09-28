66fba8cee74d29567064a9b2dcb4a9c0f44e5f2b
Aman Chulawala <aman.chulawala@gmail.com>
2023-09-27 23:29:04 -0400

    Almighty Push!

---
+++ DeepVO-main/params.py
@@ -0,0 +1,72 @@
+import os
+import torch
+
+class Parameters():
+    def __init__(self):
+
+        self.n_processors = 8
+        self.device = device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+        # Path to the dataset. Please modify this before running
+        self.data_dir = 'C:/Users/amanc/OneDrive/Desktop/787 Project/DeepVO-main/dataset'
+        self.image_dir = self.data_dir + '/sequences/'
+        self.pose_dir = self.data_dir + '/poses/'
+        
+        # List of train paths, valid paths and test paths
+        self.train_video = ['01'] #, '02', '08', '09']
+        self.valid_video = ['01']#['03', '05']
+        self.test_video = ['01']#['03', '04', '05', '06', '07', '10']
+
+        self.seq_len = 7           # Image sequence length
+        self.overlap = 1           # overlap between adjacent sampled image sequences
+
+        # Data Preprocessing
+        self.img_w = 640   # 1280 in the original paper
+        self.img_h = 192   # 384 in the original paper
+
+        # Data Augmentation (horizontal flip)
+        self.is_hflip = False
+        
+        # Neural network settings
+        self.rnn_hidden_size = 500
+        self.conv_dropout = (0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.5)
+        self.rnn_dropout_out = 0.2
+        self.rnn_dropout_between = 0.2 
+        self.batch_norm = True
+
+        # Training settings
+        self.epochs = 1
+        self.batch_size = 5
+        self.pin_mem = True
+        self.optim_lr = 1e-3
+        self.optim_decay = 5e-6
+        self.optim_lr_decay_factor = 0.1
+        self.optim_lr_step = 60
+
+        # Pretrain, Resume training
+        self.pretrained_flownet = 'flownets_bn_EPE2.459.pth.tar'
+        self.resume = False
+        self.resume_t_or_v = '.latest'
+        
+        # Paths to save and load the model
+        self.experiment_name = 'experiment_name'
+        self.save_path = 'experiments/{}'.format(self.experiment_name)
+
+        self.name = 't{}_v{}_im{}x{}_s{}_b{}'.format(''.join(self.train_video), ''.join(self.test_video), self.img_h, self.img_w, self.seq_len, self.batch_size)
+        self.name += '_flip' if self.is_hflip else ''
+
+        self.load_model_path = '{}/models/{}.model{}'.format(self.save_path, self.name, self.resume_t_or_v)
+        self.load_optimizer_path = '{}/models/{}.optimizer{}'.format(self.save_path, self.name, self.resume_t_or_v)
+        self.record_path = '{}/records/{}.txt'.format(self.save_path, self.name)
+        self.save_model_path = '{}/models/{}.model'.format(self.save_path, self.name)
+        self.save_optimzer_path = '{}/models/{}.optimizer'.format(self.save_path, self.name)
+
+        if not os.path.isdir(os.path.dirname(self.record_path)):
+            os.makedirs(os.path.dirname(self.record_path))
+        if not os.path.isdir(os.path.dirname(self.save_model_path)):
+            os.makedirs(os.path.dirname(self.save_model_path))
+        if not os.path.isdir(os.path.dirname(self.save_optimzer_path)):
+            os.makedirs(os.path.dirname(self.save_optimzer_path))
+
+
+par = Parameters()
---
+++ DeepVO-main/train.py
@@ -0,0 +1,90 @@
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+import os
+import time
+from params import par
+from model import DeepVO
+from utils.data import ImageSequenceDataset
+import math
+from torch.optim.lr_scheduler import StepLR
+
+def main():
+    # Write all hyperparameters to record_path
+    mode = 'a' if par.resume else 'w'
+    with open(par.record_path, mode) as f:
+        f.write('\n' + '=' * 50 + '\n')
+        f.write('\n'.join("%s: %s" % item for item in vars(par).items()))
+        f.write('\n' + '=' * 50 + '\n')
+
+    # Create the training set and test set
+    train_dataset = ImageSequenceDataset(par.train_video, par.seq_len, drop_last=True, overlap=par.overlap)
+    train_dl = DataLoader(train_dataset, batch_size=par.batch_size, shuffle=True, num_workers=par.n_processors, pin_memory=par.pin_mem)
+
+    valid_dataset = ImageSequenceDataset(par.valid_video, par.seq_len, drop_last=True, overlap=1)
+    valid_dl = DataLoader(valid_dataset, batch_size=par.batch_size, shuffle=True, num_workers=par.n_processors, pin_memory=par.pin_mem)
+
+    print('Number of samples in training dataset: ', len(train_dataset))
+    print('Number of samples in validation dataset: ', len(valid_dataset))
+    print('=' * 50)
+
+    # Create the model
+    M_deepvo = DeepVO(par.img_h, par.img_w, par.batch_norm)
+    M_deepvo.load_Flownet()
+    M_deepvo = M_deepvo.to(par.device)
+
+    optimizer = torch.optim.Adam(M_deepvo.parameters(), lr=par.optim_lr, betas=(0.9, 0.999), weight_decay=par.optim_decay)
+    scheduler = StepLR(optimizer, gamma=par.optim_lr_decay_factor, step_size=par.optim_lr_step)
+
+    # Load trained DeepVO model and optimizer
+    if par.resume:
+        M_deepvo.load_state_dict(torch.load(par.load_model_path))
+        optimizer.load_state_dict(torch.load(par.load_optimizer_path))
+        print('Load model from: ', par.load_model_path)
+        print('Load optimizer from: ', par.load_optimizer_path)
+
+    # Train
+    print('Record loss in: ', par.record_path)
+    min_loss_train = 1e10
+    min_loss_valid = 1e10
+
+    for ep in range(par.epochs):
+
+        st_t = time.time()
+        print('=' * 50)
+        print('Epoch ' + str(ep) + ' starts')
+
+        loss_mean_train, loss_ang_mean_train, loss_trans_mean_train = M_deepvo.train_net(train_dl, optimizer)
+        loss_mean_valid, loss_ang_mean_valid, loss_trans_mean_valid = M_deepvo.valid_net(valid_dl)
+        scheduler.step()
+        
+        message = f'Epoch {ep + 1}\ntrain loss mean: {loss_mean_train}, train ang loss mean: {loss_ang_mean_train}, train trans loss mean: {loss_trans_mean_train}, time: {time.time()-st_t} sec'
+        print(message)
+        f = open(par.record_path, 'a')
+        f.write(message+'\n')
+        
+        message = f'valid loss mean: {loss_mean_valid}, valid ang loss mean: {loss_ang_mean_valid}, valid trans loss mean: {loss_trans_mean_valid}'
+        print(message)
+        f = open(par.record_path, 'a')
+        f.write(message+'\n')
+
+        # Save model
+        check_interval = 1
+        if loss_mean_valid < min_loss_valid and ep % check_interval == 0:
+            min_loss_valid = loss_mean_valid
+            print('Save model at ep {}, mean of valid loss: {}'.format(ep + 1, loss_mean_valid))
+            torch.save(M_deepvo.state_dict(), par.save_model_path + '.valid')
+            torch.save(optimizer.state_dict(), par.save_optimzer_path + '.valid')
+
+        if loss_mean_train < min_loss_train and ep % check_interval == 0:
+            min_loss_train = loss_mean_train
+            print('Save model at ep {}, mean of train loss: {}'.format(ep + 1, loss_mean_train))
+            torch.save(M_deepvo.state_dict(), par.save_model_path + '.train')
+            torch.save(optimizer.state_dict(), par.save_optimzer_path + '.train')
+
+        print(f'Save model at ep {ep+1}')
+        torch.save(M_deepvo.state_dict(), par.save_model_path + '.latest')
+        torch.save(optimizer.state_dict(), par.save_optimzer_path + '.latest')
+
+if __name__ == '__main__':
+    main()
